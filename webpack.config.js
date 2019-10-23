09a186408d2e3d6ae466c2024b6dc2c516aae104
Kamil Piskorski <kamil.piskorski@mvpdoers.com>
2019-10-23 18:40:22 +0200

    🚀 Add editor config and meta tags for open graph.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const development = process.env.NODE_ENV === 'development';
 
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -10,102 +10,102 @@ const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
 
-    devtool: development ? 'source-map' : false,
+  devtool: development ? 'source-map' : false,
 
-    mode: development ? 'development' : 'production',
+  mode: development ? 'development' : 'production',
 
-    entry: {
-        app: './src/scripts/app.js'
-    },
+  entry: {
+    app: './src/scripts/app.js'
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js'
-    },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            '@babel/preset-env',
-                            '@babel/typescript'
-                        ]
-                    }
-                }
-            },
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets/images'
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(woff2?|ttf|otf|eot)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'assets/fonts'
-                        }
-                    }
-                ]
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images'
             }
+          }
         ]
-    },
-
-    resolve: {
-        alias: {
-            '@app': path.resolve(__dirname, 'src')
-        },
-        modules: [
-            'node_modules',
-            path.resolve(__dirname, 'src')
-        ],
-        extensions: [
-            '.js',
-            '.ts'
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/fonts'
+            }
+          }
         ]
-    },
+      }
+    ]
+  },
 
-    plugins: [
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([{
-            from: './src/assets/images',
-            to: 'assets/images'
-        }]),
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'app.[chunkhash].css',
-        })
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, 'src')
+    },
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'src')
     ],
+    extensions: [
+      '.js',
+      '.ts'
+    ]
+  },
 
-    optimization: {
-        minimizer: [
-            new UglifyJsWebpackPlugin(),
-            new OptimizeCSSAssetsWebpackPlugin()
-        ]
-    }
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([{
+      from: './src/assets/images',
+      to: 'assets/images'
+    }]),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'app.[chunkhash].css',
+    })
+  ],
+
+  optimization: {
+    minimizer: [
+      new UglifyJsWebpackPlugin(),
+      new OptimizeCSSAssetsWebpackPlugin()
+    ]
+  }
 };
